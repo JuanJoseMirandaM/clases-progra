27b807c21098676b2ceb1f73c44958f83dc5148f
Juan Jose Miranda <jjsmm97@gmail.com>
2023-08-31 20:55:34 -0400

    001 tipos de variables

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * @author Juan Jose Miranda
+ */
+public class Main {
+    public static void main(String[] args) {
+        int x = 12;
+        long y = 11234;
+        float z = 12.289329238238982388239298389289f;
+        double a = 12.2902302302930029390902390;
+        boolean f = true;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(a);
+        System.out.println(f);
+    }
+}
