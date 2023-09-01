cc08d1a7e70afcb03e57855e36abdd05f49eea4e
Juan Jose Miranda <jjsmm97@gmail.com>
2023-08-31 20:59:43 -0400

    002 operaciones aritmeticas

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,15 @@
  */
 public class Main {
     public static void main(String[] args) {
-        int x = 12;
-        long y = 11234;
-        float z = 12.289329238238982388239298389289f;
-        double a = 12.2902302302930029390902390;
-        boolean f = true;
-        System.out.println(x);
-        System.out.println(y);
-        System.out.println(z);
-        System.out.println(a);
-        System.out.println(f);
+//        int x = 12;
+//        int y = 5;
+        double x = 12;
+        double y = 5;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+        System.out.println(x % y);
     }
 }
