fb4cfcb9c8d10c3a2a78eb3144d219cdb73e7de2
Juan Jose Miranda <jjsmm97@gmail.com>
2023-09-05 20:38:32 -0400

    uso del switch

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("introduzca tres numeros");
-        int x = sc.nextInt();
-        int y = sc.nextInt();
-        int z = sc.nextInt();
-        int max = Math.max(x, Math.max(y, z));
-        System.out.println(max);
+        System.out.println("introduzca un lenguaje de programacion");
+        String x = sc.nextLine();
+        switch (x) {
+            case "java":
+                System.out.println("java$$$");
+                break;
+            case "python":
+                System.out.println("python!!!");
+                break;
+            default:
+                System.out.println("no tengo ese lenguaje");
+                break;
+        }
+
     }
 }
