428a37dd8f66bf5b0aeb9d219c7af2ea36061c9a
Juan Jose Miranda <jjsmm97@gmail.com>
2023-09-12 21:15:19 -0400

    cadenas 1

--- src/Main.java
+++ src/Main.java
@@ -6,29 +6,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        int s = t % 60;
-        t = t / 60;
-        int m = t % 60;
-        int h = t / 60;
-        System.out.printf("%02d:%02d:%02d\n", h, m, s);
+        String s = sc.nextLine();
+        System.out.println(s.charAt(0));
+        System.out.println(s.indexOf('l'));
+        System.out.println(s.length());
 
-        if (h < 10) {
-            System.out.print("0" + h + ":");
-        } else {
-            System.out.print(h + ":");
-        }
-
-        if (m < 10) {
-            System.out.print("0" + m + ":");
-        } else {
-            System.out.print(m + ":");
-        }
-
-        if (s < 10) {
-            System.out.print("0" + s);
-        } else {
-            System.out.print(s);
+        for (int i = 0; i < s.length(); i++) {
+            System.out.println(s.charAt(i));
         }
     }
 }
