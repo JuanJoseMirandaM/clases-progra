480ab7e850bc8250a55b8cebe8401a944ad09db5
Juan Jose Miranda <jjsmm97@gmail.com>
2023-09-12 21:00:20 -0400

    convertir segundos a HH:MM:SS

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double a = sc.nextDouble();
-        double b = sc.nextDouble();
-        double h = Math.sqrt(a * a + b * b);
-        System.out.format("%.2f", h);
+        int t = sc.nextInt();
+        int s = t % 60;
+        t = t / 60;
+        int m = t % 60;
+        int h = t / 60;
+        System.out.printf("%02d:%02d:%02d\n", h, m, s);
+
+        if (h < 10) {
+            System.out.print("0" + h + ":");
+        } else {
+            System.out.print(h + ":");
+        }
+
+        if (m < 10) {
+            System.out.print("0" + m + ":");
+        } else {
+            System.out.print(m + ":");
+        }
+
+        if (s < 10) {
+            System.out.print("0" + s);
+        } else {
+            System.out.print(s);
+        }
     }
 }
